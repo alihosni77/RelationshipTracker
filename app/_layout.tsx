2fded8a3343c48d400e3b82c8e51be810af62708
alihosni77 <64416137+alihosni77@users.noreply.github.com>
2026-08-25 21:51:36 +0330

    feat: add authentication route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,4 +3,4 @@ import { StatusBar } from 'expo-status-bar';
 import { I18nManager } from 'react-native';
 import { theme } from '../src/theme';
 I18nManager.allowRTL(true);
-export default function RootLayout() { return <><StatusBar style="light"/><Stack screenOptions={{headerShown:false,contentStyle:{backgroundColor:theme.colors.background}}}><Stack.Screen name="index"/><Stack.Screen name="home"/><Stack.Screen name="messages"/></Stack></>; }
+export default function RootLayout() { return <><StatusBar style="light"/><Stack screenOptions={{headerShown:false,contentStyle:{backgroundColor:theme.colors.background}}}><Stack.Screen name="index"/><Stack.Screen name="auth"/><Stack.Screen name="home"/><Stack.Screen name="messages"/></Stack></>; }
